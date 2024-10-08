56fab437e198a915973cc2abbf3e7c4d3b3c6966
Joana <144175120+diverintech@users.noreply.github.com>
2024-10-08 17:50:07 +0100

    Create the Weather Component, Service and Interface

--- src/app/modules/wheater/page/weather-home/weather-home.component.ts
+++ src/app/modules/wheater/page/weather-home/weather-home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { error } from 'console';
+import { WeatherData } from '../../../../models/interfaces/WeatherData.interface';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrl: './weather-home.component.scss'
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
 
+  initialCityName = 'Lisboa';
+  weatherData!: WeatherData;
+
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit(): void {
+    this.getWeatherData(this.initialCityName);
+  }
+
+  getWeatherData(cityName: string): void {
+    this.weatherService.getWeatherData(cityName)
+      .subscribe({
+        next: (response) => {
+          response && (this.weatherData == this.weatherData)
+          console.log(this.weatherData)
+        },
+        error: (error) => console.log(error),
+      })
+  }
 }
