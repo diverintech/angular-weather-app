ec85ec89b7b8101a41b4b467519206701abc4017
Joana <144175120+diverintech@users.noreply.github.com>
2024-10-25 14:52:46 +0100

    Update tests

--- src/app/modules/weather/services/quote/quote.service.spec.ts
+++ src/app/modules/weather/services/quote/quote.service.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { QuoteService } from './quote.service';
+import { Quote } from 'src/app/interfaces/quote.interface';
 
 describe('QuoteService', () => {
   let service: QuoteService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [QuoteService]
+    });
+
     service = TestBed.inject(QuoteService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should retrieve today\'s quote from the API', () => {
+    const mockQuote: Quote[] = [{
+      q: "Sample quote", a: "Author",
+      h: ''
+    }];
+
+    service.getTodayQuote().subscribe((quote) => {
+      expect(quote).toEqual(mockQuote);
+    });
+
+    const req = httpMock.expectOne('https://zenquotes.io/api/today');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockQuote);
   });
 });
--- src/app/modules/weather/services/weather/weather.service.spec.ts
+++ src/app/modules/weather/services/weather/weather.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { WeatherService } from './weather.service';
+import { environment } from '@env/environment';
 
 describe('WeatherService', () => {
   let service: WeatherService;
+  let httpMock: HttpTestingController;
+  const cityName = 'Lisboa';
+  const mockApiKey = environment.weatherApiKey;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${mockApiKey}`;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [WeatherService]
+    });
+
     service = TestBed.inject(WeatherService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should retrieve weather data for a specific city from the API', () => {
+    const mockWeatherData = {
+      main: { temp: 20, pressure: 1013, humidity: 60 },
+      weather: [{ description: 'clear sky', icon: '01d' }],
+      name: 'Lisboa'
+    };
+
+    service.getWeatherData(cityName).subscribe((data) => {
+      expect(data).toEqual(mockWeatherData);
+    });
+
+    const req = httpMock.expectOne(apiUrl);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockWeatherData);
   });
 });
