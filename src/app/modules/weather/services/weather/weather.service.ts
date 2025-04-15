810fba59bc592d28d59b83f56feb57e5f092c436
Joana <144175120+diverintech@users.noreply.github.com>
2025-04-15 15:31:27 +0100

    Update weather service file

--- src/app/modules/weather/services/weather/weather.service.ts
+++ src/app/modules/weather/services/weather/weather.service.ts
@@ -1,23 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs'
-import {environment} from "@env/environment";
-
+import { environment } from '@env/environment.mock';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
-
   private apiKey = environment.weatherApiKey;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getWeatherData(cityName: string): Observable<any> {
     return this.http.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&appid=${this.apiKey}`,
-      {});
+      {}
+    );
   }
 }
-
-
